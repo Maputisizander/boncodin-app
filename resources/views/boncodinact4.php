db9e225b5775c3038c2658166e4923a4bc9e68d6
Zander <chowardpaknoy1930@gmail.com>
2025-02-15 17:15:50 +0800

    initial commit

---
+++ resources/views/boncodinact4.php
@@ -0,0 +1,25 @@
+
+<!-- saved from url=(0025)http://localhost/test.php -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<link rel="stylesheet" type="text/css" href="./boncodinact4_files/design.css">
+</head>
+<body>
+
+<h2>Enter Multiplier, Multiplicand, and Surname</h2>
+
+
+ <form action="http://localhost/test.php" method="POST">
+    <label for="multiplier">Multiplier:</label>
+    <input type="number" id="multiplier" name="multiplier" value="0" required=""><br><br>
+    
+    <label for="multiplicand">Multiplicand:</label>
+    <input type="number" id="multiplicand" name="multiplicand" value="0" required=""><br><br>
+    
+    <label for="surname">Surname:</label>
+    <input type="text" id="surname" name="surname" value="boncodin" required=""><br><br>
+    
+    <input type="submit" value="Generate Table">
+</form> 
+
+<table border="5" style="border-collapse: collapse; text-align: center;"><tbody><tr><th></th><th>1</th><th>2</th><th>3</th><th>4</th><th>5</th><th>6</th><th>7</th><th>8</th><th>9</th><th>10</th><th>11</th><th>12</th></tr><tr><th>1</th><td>1boncodin</td><td>2</td><td>3boncodin</td><td>4</td><td>5boncodin</td><td>6</td><td>7boncodin</td><td>8</td><td>9boncodin</td><td>*0</td><td>11boncodin</td><td>*2</td></tr><tr><th>2</th><td>2</td><td>4</td><td>6</td><td>8</td><td>*0</td><td>*2</td><td>*4</td><td>*6</td><td>*8</td><td>*0</td><td>*2</td><td>*4</td></tr><tr><th>3</th><td>3boncodin</td><td>6</td><td>9boncodin</td><td>*2</td><td>15boncodin</td><td>*8</td><td>21boncodin</td><td>*4</td><td>27boncodin</td><td>*0</td><td>33boncodin</td><td>*6</td></tr><tr><th>4</th><td>4</td><td>8</td><td>*2</td><td>*6</td><td>*0</td><td>*4</td><td>*8</td><td>*2</td><td>*6</td><td>*0</td><td>*4</td><td>*8</td></tr><tr><th>5</th><td>5boncodin</td><td>*0</td><td>15boncodin</td><td>*0</td><td>25boncodin</td><td>*0</td><td>35boncodin</td><td>*0</td><td>45boncodin</td><td>*0</td><td>55boncodin</td><td>*0</td></tr><tr><th>6</th><td>6</td><td>*2</td><td>*8</td><td>*4</td><td>*0</td><td>*6</td><td>*2</td><td>*8</td><td>*4</td><td>*0</td><td>*6</td><td>*2</td></tr><tr><th>7</th><td>7boncodin</td><td>*4</td><td>21boncodin</td><td>*8</td><td>35boncodin</td><td>*2</td><td>49boncodin</td><td>*6</td><td>63boncodin</td><td>*0</td><td>77boncodin</td><td>*4</td></tr><tr><th>8</th><td>8</td><td>*6</td><td>*4</td><td>*2</td><td>*0</td><td>*8</td><td>*6</td><td>*4</td><td>*2</td><td>*0</td><td>*8</td><td>*6</td></tr><tr><th>9</th><td>9boncodin</td><td>*8</td><td>27boncodin</td><td>*6</td><td>45boncodin</td><td>*4</td><td>63boncodin</td><td>*2</td><td>81boncodin</td><td>*0</td><td>99boncodin</td><td>**8</td></tr><tr><th>10</th><td>*0</td><td>*0</td><td>*0</td><td>*0</td><td>*0</td><td>*0</td><td>*0</td><td>*0</td><td>*0</td><td>**0</td><td>**0</td><td>**0</td></tr><tr><th>11</th><td>11boncodin</td><td>*2</td><td>33boncodin</td><td>*4</td><td>55boncodin</td><td>*6</td><td>77boncodin</td><td>*8</td><td>99boncodin</td><td>**0</td><td>121boncodin</td><td>**2</td></tr><tr><th>12</th><td>*2</td><td>*4</td><td>*6</td><td>*8</td><td>*0</td><td>*2</td><td>*4</td><td>*6</td><td>**8</td><td>**0</td><td>**2</td><td>**4</td></tr><tr><th>13</th><td>13boncodin</td><td>*6</td><td>39boncodin</td><td>*2</td><td>65boncodin</td><td>*8</td><td>91boncodin</td><td>**4</td><td>117boncodin</td><td>**0</td><td>143boncodin</td><td>**6</td></tr></tbody></table>
+</body></html>
